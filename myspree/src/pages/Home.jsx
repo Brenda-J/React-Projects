c2d08602f03d218a0f8021f557f02dc7e056160f
Brenda J <linjiangda@yahoo.com>
2021-09-21 22:32:17 -0400

    Worked on navbar

--- myspree/src/pages/Home.jsx
+++ myspree/src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Navbar from "../components/Navbar";
 
-export const Home = () => {
+const Home = () => {
     return (
         <div>
-            
+            <Navbar />
         </div>
     )
 }
+
+export default Home;
