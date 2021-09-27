2219971a7b912fd30662798809c554e25ba9d98f
Brenda J <linjiangda@yahoo.com>
2021-09-26 23:06:11 -0400

    worked on slides

--- myspree/src/components/Slider.jsx
+++ myspree/src/components/Slider.jsx
@@ -1,94 +1,120 @@
-import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
-import React from 'react'
-import styled from 'styled-components';
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { slideItems } from "../slides";
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    display: flex;
-    position: relative;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
-    width: 50px;
-    height: 50px;
-    background-color: #fff8f8;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: ${props => props.direction === "left" && "10px"};
-    right: ${props => props.direction === "right" && "10px"};
-    margin: auto;
-    cursor: pointer;
-    opacity: 0.5;
+  width: 50px;
+  height: 50px;
+  background-color: #fff8f8;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
-    height: 100%;
-`
+  height: 100%;
+  display: flex;
+  transform: translateX(${props => props.slideIndex * -100}vw);
+  transition: all 1.2s ease;
+`;
 
 const Slide = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100vw;
-    height: 100vh;
-`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #${(props) => props.bgcolor};
+`;
 
 const ImageContainer = styled.div`
-    flex: 1;
-    height: 100%;
-`
+  flex: 1;
+  height: 100%;
+`;
 
 const InfoContainer = styled.div`
-    flex: 1;
-    padding: 50px;
-    z-index: 1;
-`
+  flex: 1;
+  padding: 50px;
+  z-index: 1;
+`;
 
 const Title = styled.h1`
-    font-size: 80px;
-`
+  font-size: 80px;
+`;
 
 const Description = styled.p`
-    margin: 50px 0px;
-    font-size: 20px;
-`
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
 
 const Button = styled.button`
-    
-`
+  cursor: pointer;
+  padding: 10px;
+  font-size: 18px;
+`;
 
 const Image = styled.img`
-    height: 80%;
-`
+  height: 80%;
+`;
 
 const Slider = () => {
-    return (
-        <Container>
-            <Arrow direction="left">
-               <ArrowLeftOutlined /> 
-            </Arrow>
-            <Wrapper>
-                <Slide>
-                <ImageContainer>
-                    <Image src="https://images.squarespace-cdn.com/content/v1/5c109eb3a2772c5895c5cb30/1554004392333-IA0SA4N24HJHL3CF4GEB/fotografo+e-commerce.jpg"/>
-                </ImageContainer>
-                <InfoContainer>
-                    <Title>Fall Sale</Title>
-                    <Description>Get your seasonal clothing here! 50% OFF</Description>
-                    <Button>GET MY DISCOUNT</Button>
-                </InfoContainer>
-                </Slide>
-            </Wrapper>
-            <Arrow direction="right">
-               <ArrowRightOutlined /> 
-            </Arrow>
-        </Container>
-    )
-}
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {
+
+    if (direction === "left") {
+        setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 3)
+    } else {
+        setSlideIndex(slideIndex < 3 ? slideIndex + 1 : 0)
+    }
+  };
+
+  return (
+    <Container>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {slideItems.map((item) => (
+          <Slide bgcolor={item.bgcolor}>
+            <ImageContainer>
+              <Image src={item.image} />
+            </ImageContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>
+                  {item.description}
+              </Description>
+              <Button>GET MY DISCOUNT NOW!</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
 
 export default Slider;
---
+++ myspree/src/slides.js
@@ -0,0 +1,30 @@
+export const slideItems = [
+    {
+        id: 1,
+        image: "https://images.squarespace-cdn.com/content/v1/5c109eb3a2772c5895c5cb30/1554004392333-IA0SA4N24HJHL3CF4GEB/fotografo+e-commerce.jpg",
+        title: "WINTER SALE",
+        description: "Best Winter sale ever!",
+        bgcolor: "F0FFFF"
+    },
+    {
+        id: 1,
+        image: "https://images.squarespace-cdn.com/content/v1/5c109eb3a2772c5895c5cb30/1554004392333-IA0SA4N24HJHL3CF4GEB/fotografo+e-commerce.jpg",
+        title: "SPRING SALE",
+        description: "Best SPRING sale ever!",
+        bgcolor: "7FFFD4"
+    },
+    {
+        id: 1,
+        image: "https://images.squarespace-cdn.com/content/v1/5c109eb3a2772c5895c5cb30/1554004392333-IA0SA4N24HJHL3CF4GEB/fotografo+e-commerce.jpg",
+        title: "SUMMER SALE",
+        description: "Best SUMMER sale ever!",
+        bgcolor: "FFF0F5"
+    },
+    {
+        id: 1,
+        image: "https://images.squarespace-cdn.com/content/v1/5c109eb3a2772c5895c5cb30/1554004392333-IA0SA4N24HJHL3CF4GEB/fotografo+e-commerce.jpg",
+        title: "FALL SALE",
+        description: "Best FALL sale ever!",
+        bgcolor: "E6E6FA"
+    }
+]
